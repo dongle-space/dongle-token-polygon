2344d9f7f8ae0dd0195254c015ad9aaf3c7a15e3
Seokjung Kim <88718547+seokjungms@users.noreply.github.com>
2022-03-03 21:27:47 +0900

    change deploy contracts file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
+const { deployProxy } = require('@openzeppelin/truffle-upgrades');
+
 const Dongle = artifacts.require("./Dongle.sol");
 
-module.exports = function (deployer) {
-  deployer.deploy(Dongle);
+module.exports = async function (deployer) {
+  const instance = await deployProxy(Dongle, {deployer});
+  console.log('Deployed', instance.address);
 };
